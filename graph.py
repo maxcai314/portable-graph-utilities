550a7c33d5f403c3f3a321a7cdd80432c097436f
Max Cai <maxcai.paloalto@gmail.com>
2024-07-13 09:47:42 -0700

    graph.py

---
+++ graph.py
@@ -0,0 +1,78 @@
+class Node:
+    def __init__(self):
+        self._edges = set()
+
+    def edges(self):
+        return iter(self._edges)
+
+    def _register_edge(self, edge):
+        self._edges.add(edge)
+
+    def _deregister_edge(self, edge):
+        self._edges.discard(edge)
+
+
+class Edge:
+    def __init__(self, source: Node, target: Node):
+        self._source = source
+        self._target = target
+
+    @property
+    def source(self):
+        return self._source
+
+    @property
+    def target(self):
+        return self._target
+
+    def __repr__(self) -> str:
+        return f"({self.source} -> {self.target})"
+
+
+class Graph:
+    def __init__(self):
+        self._nodes = set()
+        self._edges = {}
+
+    def add_node(self, node: Node):
+        self._nodes.add(node)
+
+    def remove_node(self, node: Node):
+        for edge in node.edges():
+            self.remove_edge(edge)
+        self._nodes.remove(node)
+
+    def has_node(self, node: Node):
+        return node in self._nodes
+
+    def nodes(self):
+        return iter(self._nodes)
+
+    def add_edge(self, edge: Edge):
+        if self.has_edge(edge):
+            raise ValueError("Edge already exists between nodes")
+
+        edge.source._register_edge(edge)
+        edge.target._register_edge(edge)
+        self._edges[edge.source, edge.target] = edge
+
+    def remove_edge(self, edge: Edge):
+        edge.source._deregister_edge(edge)
+        edge.target._deregister_edge(edge)
+        self._edges.pop((edge.source, edge.target), None)
+        self._edges.pop((edge.target, edge.source), None)
+
+    def has_edge(self, edge: Edge):
+        return self.has_edge_between(edge.source, edge.target)
+
+    def has_edge_between(self, source: Node, target: Node):
+        return (source, target) in self._edges or (target, source) in self._edges
+
+    def edge_between(self, source: Node, target: Node):
+        return self._edges.get((source, target), None) or self._edges.get((target, source), None)
+
+    def edges(self):
+        return self._edges.values()
+
+    def __repr__(self) -> str:
+        return f"Graph(nodes={self._nodes}, edges={self._edges.values()})"
---
+++ test_graph.py
@@ -0,0 +1,132 @@
+from graph import Node, Edge, Graph
+import unittest
+
+
+class NamedNode(Node):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+
+    def __repr__(self) -> str:
+        return f"{self.name}"
+
+
+class NamedEdge(Edge):
+    def __init__(self, source: NamedNode, target: NamedNode, name):
+        super().__init__(source, target)
+        self.name = name
+
+    def __repr__(self) -> str:
+        return f"{self.name}({self.source} -> {self.target})"
+
+
+class TestGraph(unittest.TestCase):
+    def test_graph(self):
+        graph = Graph()
+        node1 = Node()
+        node2 = Node()
+        edge = Edge(node1, node2)
+
+        graph.add_node(node1)
+        graph.add_node(node2)
+
+        graph.add_edge(edge)
+
+        with self.assertRaises(ValueError):
+            graph.add_edge(edge)
+
+        self.assertTrue(graph.has_node(node1))
+        self.assertTrue(graph.has_node(node2))
+
+        self.assertTrue(edge in node1.edges())
+        self.assertTrue(edge in node2.edges())
+
+        self.assertTrue(graph.has_edge_between(node1, node2))
+        self.assertTrue(graph.has_edge(edge))
+        self.assertTrue(edge == graph.edge_between(node1, node2))
+        self.assertTrue(edge == graph.edge_between(node2, node1))
+
+        print(list(graph.edges()))
+        print(list(graph.nodes()))
+        print(graph)
+
+        graph.remove_edge(edge)
+
+        self.assertFalse(graph.has_edge(edge))
+        self.assertFalse(graph.has_edge_between(node1, node2))
+        self.assertFalse(graph.has_edge_between(node2, node1))
+        self.assertFalse(edge in node1.edges())
+        self.assertFalse(edge in node2.edges())
+
+        self.assertTrue(graph.has_node(node1))
+        self.assertTrue(graph.has_node(node2))
+
+        graph.remove_node(node1)
+        self.assertFalse(graph.has_node(node1))
+        self.assertTrue(graph.has_node(node2))
+        graph.remove_node(node2)
+        self.assertFalse(graph.has_node(node2))
+        self.assertFalse(graph.has_node(node1))
+
+        assert not graph.has_node(node1)
+        assert not graph.has_node(node2)
+
+        print([e for e in graph.edges()])
+        print([n for n in graph.nodes()])
+
+    def test_custom_graph(self):
+        graph = Graph()
+        node1 = NamedNode("Alice")
+        node2 = NamedNode("Bob")
+        edge = NamedEdge(node1, node2, "Edge1")
+
+        graph.add_node(node1)
+        graph.add_node(node2)
+
+        graph.add_edge(edge)
+
+        with self.assertRaises(ValueError):
+            graph.add_edge(edge)
+
+        self.assertTrue(graph.has_node(node1))
+        self.assertTrue(graph.has_node(node2))
+
+        self.assertTrue(edge in node1.edges())
+        self.assertTrue(edge in node2.edges())
+
+        self.assertTrue(graph.has_edge_between(node1, node2))
+        self.assertTrue(graph.has_edge(edge))
+        self.assertTrue(edge == graph.edge_between(node1, node2))
+        self.assertTrue(edge == graph.edge_between(node2, node1))
+
+        print(list(graph.edges()))
+        print(list(graph.nodes()))
+        print(graph)
+
+        graph.remove_edge(edge)
+
+        self.assertFalse(graph.has_edge(edge))
+        self.assertFalse(graph.has_edge_between(node1, node2))
+        self.assertFalse(graph.has_edge_between(node2, node1))
+        self.assertFalse(edge in node1.edges())
+        self.assertFalse(edge in node2.edges())
+
+        self.assertTrue(graph.has_node(node1))
+        self.assertTrue(graph.has_node(node2))
+
+        graph.remove_node(node1)
+        self.assertFalse(graph.has_node(node1))
+        self.assertTrue(graph.has_node(node2))
+        graph.remove_node(node2)
+        self.assertFalse(graph.has_node(node2))
+        self.assertFalse(graph.has_node(node1))
+
+        assert not graph.has_node(node1)
+        assert not graph.has_node(node2)
+
+        print([e for e in graph.edges()])
+        print([n for n in graph.nodes()])
+
+
+if __name__ == "__main__":
+    unittest.main()
